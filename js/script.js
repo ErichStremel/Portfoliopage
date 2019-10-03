eef298eb1cb8f97fae57890768a5dd805dc3c079
Erich Stremel <erichstremel@gmail.com>
2019-10-03 14:05:40 -0600

    Added profile section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-// when document is ready, load the slider
+// when document is ready, load
 $(document).ready(function(){
     $('#slides').superslides({
         animation: 'fade',
         play: 4000, 
-    })
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Web Developer", "Front End Programmer", "Passionate Professional"], 
+        typeSpeed: 70, 
+        loop: true,
+        startDelay: 1000, 
+        showCursor: false
+    });
+
 });
