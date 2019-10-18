a6315ff6fe082f8b97b247c2707c6e178baba8e7
Erich Stremel <erichstremel@gmail.com>
2019-10-18 14:11:43 -0600

    Added portfolio section

--- js/script.js
+++ js/script.js
@@ -50,5 +50,17 @@ $(document).ready(function () {
             });
         }
 
+    });
+
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
     })
+
 });
